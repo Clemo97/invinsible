8559b1032745e848e563b5260d784869dc2ff605
Clemo97 <lumumbaclement@gmail.com>
2022-06-01 22:50:08 +0300

    change debug setting

--- gallery/settings.py
+++ gallery/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', False)
 
 # development
 if config('MODE')=="dev":
